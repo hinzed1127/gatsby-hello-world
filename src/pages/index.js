24ec402a547526c7c32e4a52aa496862a5a599fa
Dan Hinze <hinzed1127@gmail.com>
2018-09-25 22:15:18 -0500

    Add page that copies CSS modules, but uses styled components instead

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,24 +3,30 @@ import { Link } from "gatsby";
 import Header from "../components/header";
 
 export default () => (
-    <>
-        <Header text="Yooooooo" />
-        <Header text="This is my website" />
-        <div>
-            <Link to="/about/">About</Link>
-        </div>
-        <div>
-            <Link to="/dogs/">Dogs</Link>
-        </div>
-        <div>
-            <Link to="/typography/">Here's what typography.js does</Link>
-        </div>
-        <div>
-            <Link to="/about-css-modules/">Here's what CSS Modules do</Link>
-        </div>
+  <>
+    <Header text="Yooooooo" />
+    <Header text="This is my website" />
+    <div>
+      <Link to="/about/">About</Link>
+    </div>
+    <div>
+      <Link to="/dogs/">Dogs</Link>
+    </div>
+    <div>
+      <Link to="/typography/">Here's what typography.js does</Link>
+    </div>
+    <div>
+      <Link to="/about-css-modules/">Here's what CSS Modules do</Link>
+    </div>
+    <div>
+      <Link to="/about-styled-components/">Here's what styled components do</Link>
+    </div>
 
-        <div style={{color: 'purple', fontSize: '72px'}}>Hello MPLS!</div>
-        <div style={{ color: 'blue', fontSize: '36px' }}>(Also, Hello world.)</div>
-        <img src="https://source.unsplash.com/random/800x400" alt="something random" />
-    </>
+    <div style={{ color: "purple", fontSize: "72px" }}>Hello MPLS!</div>
+    <div style={{ color: "blue", fontSize: "36px" }}>(Also, Hello world.)</div>
+    <img
+      src="https://source.unsplash.com/random/800x400"
+      alt="something random"
+    />
+  </>
 );
